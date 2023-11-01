f4ccb1e6db24d5f33aa12149d53513b41b566dd0
Vuk Topalovic <63306506+vukkt@users.noreply.github.com>
2023-11-01 17:12:20 +0100

    Add files via upload

---
+++ src/test/java/com/ed/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ed.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+//@EnableEurekaClient
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
